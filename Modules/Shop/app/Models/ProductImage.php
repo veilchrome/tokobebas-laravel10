c6a9668ef8ef1ea76de79d03761f4a3f3a5ab585
veilchrome <keeita768900@gmail.com>
2024-01-13 09:52:15 +0700

    Projek TokoBebas Laravel

--- Modules/Shop/app/Models/ProductImage.php
+++ Modules/Shop/app/Models/ProductImage.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Shop\Database\factories\ProductImageFactory;
 
+
 class ProductImage extends Model
 {
     use HasFactory, UuidTrait;
--- Modules/Shop/app/Models/ProductInventory.php
+++ Modules/Shop/app/Models/ProductInventory.php
@@ -5,17 +5,25 @@ namespace Modules\Shop\app\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Shop\Database\factories\ProductInventoryFactory;
+use App\Traits\UuidTrait;
 
 class ProductInventory extends Model
 {
-    use HasFactory;
+    use HasFactory, UuidTrait;
 
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = [];
+    protected $table = 'shop_product_inventories';
+
+    protected $fillable = [
+        'product_id',
+        'qty',
+        'low_stock_threshold',
+    ];
 
     protected static function newFactory()
     {
+        return \Modules\Shop\Database\factories\ProductInventoryFactory::new();
     }
 }
